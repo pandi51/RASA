4edac09130d7ab61abd13864c651404da646e6c4
pandi <chellapandi51@gmail.com>
2020-04-16 18:03:59 +0530

    initial commit

---
+++ actions.py
@@ -0,0 +1,66 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List, Union
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import AllSlotsReset
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormAction
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
+
+class ActionCovidMailForm(FormAction):
+
+    def name(self) -> Text:
+        return "covid_mail_form"
+
+    @staticmethod
+    def required_slots(tracker: Tracker) -> List[Text]:
+        return ["NAME", "CITY", "MAIL"]
+
+    def submit(self, dispatcher: CollectingDispatcher,
+               tracker: Tracker, domain: Dict[Text, any])->List[Dict]:
+        name = tracker.get_slot('NAME')
+        city = tracker.get_slot('CITY')
+        mail = tracker.get_slot('MAIL')
+        dispatcher.utter_message(template="utter_mail_send",
+                                 name = name,
+                                 city = city,
+                                 mail = mail)
+        print(name, city, mail)
+        tracker.set('NAME')
+        return []
+
+    def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
+        print("slot_mapping invoked")
+        return {"name": [self.from_entity(entity="NAME", intent="my_name"),
+                         self.from_text()],
+                "city": [self.from_entity(entity="CITY", intent="city"),
+                         self.from_text()],
+                "mail": [self.from_entity(entity="MAIL", intent="mail"),
+                         self.from_text()]}
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, any]) -> List[Dict[Text, any]]:
+        print("run method covid")
+        return [AllSlotsReset()]
---
+++ data/nlu.md
@@ -0,0 +1,71 @@
+## intent:greet
+- hey
+- hello
+- hi
+- good morning
+- good evening
+- hey there
+
+## intent:goodbye
+- bye
+- goodbye
+- see you around
+- see you later
+
+## intent:affirm
+- yes
+- indeed
+- of course
+- that sounds good
+- correct
+
+## intent:deny
+- no
+- never
+- I don't think so
+- don't like that
+- no way
+- not really
+
+## intent:mood_great
+- perfect
+- very good
+- great
+- amazing
+- wonderful
+- I am feeling very good
+- I am great
+- I'm good
+
+## intent:mood_unhappy
+- sad
+- very sad
+- unhappy
+- bad
+- very bad
+- awful
+- terrible
+- not very good
+- extremely sad
+- so sad
+
+## intent:bot_challenge
+- are you a bot?
+- are you a human?
+- am I talking to a bot?
+- am I talking to a human?
+
+## intent:covid_detail
+- covid detail in my area?
+- covid status?
+
+## intent: my_name
+- my name is [pandi](NAME:pandi)
+- name [pandi](NAME:pandi)
+
+## intent: city
+- my city is [chennai](CITY:chennai)
+- city [chennai](CITY:chennai)
+
+## intent: mail
+- mail id [pandi@gmail.com](MAIL:pandi@gmail.com)
